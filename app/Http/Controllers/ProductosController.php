bf3be42bddd4607465b5f757aaf6644d3990111f
Pablo Aristizabal <radabeca23@gmail.com>
2024-05-17 15:55:45 -0500

    Creacion controlador de productos y routing

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    public function find() {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JsonModel  $jsonModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update()
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param  \App\Models\JsonModel  $jsonModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('productos')->group(function () {
+    Route::get('', [ProductosController::class, 'index']);
+    Route::post('', [ProductosController::class, 'store']);
+    Route::get('/{id}', [ProductosController::class, 'find']);
+    Route::put('/{id}', [ProductosController::class, 'update']);
+    Route::delete('/{id}', [ProductosController::class, 'destroy']);
 });
